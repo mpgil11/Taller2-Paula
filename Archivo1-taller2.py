15f2c01cc1041ea9a06cf34097664efd310c74f4
Sara Gonzalez <ss.gonzalezc1@uniandes.edu.co>
2025-02-03 18:49:22 -0500

    grafica histograma

--- Archivo1-taller2.py
+++ Archivo1-taller2.py
@@ -1,7 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 mu = 5
 sigma = 0.6
 n=1000
 vals = np.random.normal(loc=mu, scale=sigma, size=n)
-print(vals)
+
+plt.hist(vals, bins=30, color='skyblue', edgecolor='black')
+plt.xlabel('Valor')
+plt.ylabel('Frecuencia')
+plt.title('Histograma de datos')
+plt.grid(axis='y', alpha=0.75)
